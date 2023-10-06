fdc44389e78ed5cba4aa30ef70ef7d73da4438a3
EsmaEsme <145126288+EsmaEsme@users.noreply.github.com>
2023-10-06 15:42:37 +0300

    Add files via upload

---
+++ U15/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(textBox1.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.10; 
+            label1.Text = indirimliFiyat.ToString();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(textBox1.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.25; 
+            label1.Text = indirimliFiyat.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(textBox1.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.50; 
+            label1.Text = indirimliFiyat.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(textBox1.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.75;
+            label1.Text = indirimliFiyat.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
